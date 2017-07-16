c35f861523ec02d79f3c1521ea2549393a4a3044
jemin <jemin@uoregon.edu>
2017-07-16 06:23:05 -0700

    lab5

---
+++ lab5/TideTable/app/src/main/java/com/example/jeminson/tidetable/RSSFeed.java
@@ -0,0 +1,14 @@
+package com.example.jeminson.tidetable;
+
+/**
+ * Created by jeminson on 2017. 7. 16..
+ */
+
+import java.util.ArrayList;
+
+public class RSSFeed extends ArrayList<RSSItem> {
+    // Extending ArrayList to facilitate possible future features
+
+    // Default Serial ID
+    private static final long serialVersionUID = 1L;
+}
