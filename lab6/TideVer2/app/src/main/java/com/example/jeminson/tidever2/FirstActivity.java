f29f9a6e6770a97ee3a7ec186f514ca2ff8508b6
jemin <jemin@uoregon.edu>
2017-07-17 05:01:30 -0700

    added date picker

--- lab6/TideVer2/app/src/main/java/com/example/jeminson/tidever2/FirstActivity.java
+++ lab6/TideVer2/app/src/main/java/com/example/jeminson/tidever2/FirstActivity.java
@@ -1,20 +1,69 @@
 package com.example.jeminson.tidever2;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.ReceiverCallNotAllowedException;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class FirstActivity extends AppCompatActivity implements OnClickListener{
 
+    private static final String TAG = "FirstActivity";
+
+    private TextView mDisyplayDate;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+
     private Button showTideButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_activity);
+
+        mDisyplayDate = (TextView) findViewById(R.id.mDisyplayDate);
+
+        mDisyplayDate.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        FirstActivity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        dateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisyplayDate.setText(date);
+            }
+        };
+
 
         showTideButton = (Button) findViewById(R.id.showTideButton);
         showTideButton.setOnClickListener(this);
