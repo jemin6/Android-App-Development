d3f003ff53848c7f7672f4f5fd7c68b3edeaa4b8
jemin <jemin@uoregon.edu>
2017-07-19 18:55:11 -0700

    init

---
+++ lab7/TideVer3/app/src/main/java/com/example/jeminson/tidever3/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.jeminson.tidever3;
+
+/**
+ * Created by jeminson on 2017. 7. 19..
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+
+}
