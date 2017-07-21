4b3c436b72601f4a52a23173cda798caec0187fc
jemin <jemin@uoregon.edu>
2017-07-21 03:02:06 -0700

    TideItem, TideItems, TideSQLiteHelper file init, but not applied yet

--- lab7/TideVer3/app/src/main/java/com/example/jeminson/tidever3/TideItem.java
+++ lab7/TideVer3/app/src/main/java/com/example/jeminson/tidever3/TideItem.java
@@ -6,7 +6,36 @@ package com.example.jeminson.tidever3;
 
 import android.annotation.SuppressLint;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SuppressLint("SimpleDateFormat")
 public class TideItem {
+
+    private String tideDate = null;
+    private String time = null;
+    private String pred = null;
+    private String type = null;
+
+    public void setTideDate(String tideDate) {
+        this.tideDate = tideDate;
+    }
+    public String getTideDate() { return tideDate; }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+    public String getTime() { return time; }
+
+    public void setPred(String pred) {
+        this.pred = pred;
+    }
+    public String getPred() { return pred; }
+
+    public void setType(String type) {
+        this.tideDate = type;
+    }
+    public String getType() { return type; }
 
 } // End TideItem
--- lab7/TideVer3/app/src/main/java/com/example/jeminson/tidever3/TideSQLiteHelper.java
+++ lab7/TideVer3/app/src/main/java/com/example/jeminson/tidever3/TideSQLiteHelper.java
@@ -13,8 +13,12 @@ public class TideSQLiteHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "tide.sqlite";
     private static final int DATABASE_VERSION = 1;
 
-    public static final String ITEM_TABLE = "item";
-
+    public static final String HIGH_LOW_VALUES = "HighLowValues";
+    public static final String ITEM = "item";
+    public static final String DATA = "data";
+    public static final String TIME = "time";
+    public static final String PRED = "pred";
+    public static final String TYPE = "type";
 
 
     private Context context = null;
@@ -26,10 +30,13 @@ public class TideSQLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + ITEM_TABLE
-                    );
+        db.execSQL("CREATE TABLE " + DATA
+                + "( _id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + TIME + " TEXT,"
+                + PRED + " INTEGER,"
+                + TYPE + " TEXT,"
+                + ")" );
     }
-
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
