9b21f169c57749ca054774c5e049acda8dc52a0f
jemin <jemin@uoregon.edu>
2017-07-22 02:43:10 -0700

    only one player so that he plays with computer

---
+++ termProject/TicTacToe/app/src/main/java/com/example/jeminson/tictactoe/RockPaperScissorsGame.java
@@ -0,0 +1,14 @@
+package com.example.jeminson.tictactoe;
+
+import java.util.Random;
+
+/**
+ * Created by jeminson on 2017. 7. 22..
+ */
+
+public class RockPaperScissorsGame {
+
+    //Instance Variables
+    private Random random = new Random();
+
+}
