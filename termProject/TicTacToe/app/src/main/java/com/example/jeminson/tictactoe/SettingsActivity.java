d36550cb255679942073f6100c20a913b366d2b7
jemin <jemin@uoregon.edu>
2017-07-22 02:27:08 -0700

    layout + icon set

--- termProject/TicTacToe/app/src/main/java/com/example/jeminson/tictactoe/SettingsActivity.java
+++ termProject/TicTacToe/app/src/main/java/com/example/jeminson/tictactoe/SettingsActivity.java
@@ -4,10 +4,10 @@ package com.example.jeminson.tictactoe;
  * Created by jeminson on 2017. 7. 21..
  */
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
